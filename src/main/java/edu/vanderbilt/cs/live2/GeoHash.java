516df1573544cccd7bfb02f64ec1db7aea0390fb
Jules White <jules.white@gmail.com>
2020-08-27 07:08:41 -0500

    Assignment Commit

---
+++ src/main/java/edu/vanderbilt/cs/live2/GeoHash.java
@@ -0,0 +1,166 @@
+package edu.vanderbilt.cs.live2;
+
+
+
+public class GeoHash {
+
+	/**
+	 * This first live session will focus on basic Java and some concepts important to
+	 * functional programming, such as recursion.
+	 *
+	 * This class uses a main() method where we can write our own simple "experiments" to
+	 * test how our code works. You are encouraged to modify the main method to play around
+	 * with your code and understand it. When you have working code, you can extract it
+	 * into a method. When you have working examples with assertions, you can extract them
+	 * into tests.
+	 *
+	 * I have left some sample experiments in main() to help you understand the geohash
+	 * algorithm.
+	 *
+	 *
+	 * This class will provide an implementation of GeoHashes:
+	 *
+	 * https://www.mapzen.com/blog/geohashes-and-you/
+	 * https://en.wikipedia.org/wiki/Geohash
+	 *
+	 *
+	 * GeoHash Spatial Precision:
+	 *
+	 * https://releases.dataone.org/online/api-documentation-v2.0.1/design/geohash.html
+	 */
+
+
+	public static final double[] LATITUDE_RANGE = { -90, 90 };
+	public static final double[] LONGITUDE_RANGE = { -180, 180 };
+
+	public static boolean[] geohash1D(double valueToHash, double[] valueRange, int bitsOfPrecision) {
+		// It may help you to print out what is happening:
+		//
+		// System.out.println("hash " + valueToHash +" [" + valueRange[0] +"," +
+		// valueRange[1] + "]");
+
+		// @ToDo:
+		//
+		// Implement GeoHashing with a recursive algorithm for a single value (NOT! a
+		// latitude, longitude pair).
+		//
+		// For now, this method only needs to "geohash" either latitute or longitude
+		// separately.
+		//
+		// You will be passed a valueToHash and a valueRange (e.g., the range of
+		// longitudes or latitudes).
+		//
+		// The bits of precision is the number of bits that should be in your output
+		// hash.
+		//
+		// We are approximating "bits" with a boolean array to make things simpler.
+		//
+		return null;
+	}
+
+	public static boolean[] geohash2D(double v1, double[] v1range, double v2, double[] v2range, int bitsOfPrecision) {
+
+		// @ToDo:
+		//
+		// Separately compute indvidual 1D geohashes for v1 and v2 and then interleave them together
+		// into a final combined geohash.
+		//
+		// The resulting geohash should have the number of bits specified by bitsOfPrecision.
+		//
+
+		return null;
+	}
+
+	public static boolean[] geohash(double lat, double lon, int bitsOfPrecision) {
+		return geohash2D(lat, LATITUDE_RANGE, lon, LONGITUDE_RANGE, bitsOfPrecision);
+	}
+
+	// This is a helper method that will make printing out
+	// geohashes easier
+	public static String toHashString(boolean[] geohash) {
+		String hashString = "";
+		for (boolean b : geohash) {
+			hashString += (b ? "1" : "0");
+		}
+		return hashString;
+	}
+
+	// This is a convenience method to make it easy to get a string of 1s and 0s for a
+	// geohash
+	public static String geohashString(double valueToHash, double[] valueRange, int bitsOfPrecision) {
+		return toHashString(geohash1D(valueToHash,valueRange,bitsOfPrecision));
+	}
+
+	// Faux testing for now
+	public static void assertEquals(String v1, String v2) {
+		if(!v1.contentEquals(v2)) {
+			throw new RuntimeException(v1 + " != " + v2);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		// Example of hand-coding a 3-bit geohash
+
+		// 1st bit of the geohash
+		double longitude = -0.0;
+		double[] bounds = {LATITUDE_RANGE[0], LATITUDE_RANGE[1]};
+		double midpoint = (bounds[0] + bounds[1]) / 2;
+		boolean bit = false;
+
+		if (longitude >= midpoint) {
+			bit = true;
+			bounds[0] = midpoint;
+		}
+		else {
+			bit = false;
+			bounds[1] = midpoint;
+		}
+
+		// 2nd bit of the geohash
+		boolean bit2 = false;
+		midpoint = (bounds[0] + bounds[1]) / 2;
+		if (longitude >= midpoint) {
+			bit2 = true;
+			bounds[0] = midpoint;
+		}
+		else {
+			bit2 = false;
+			bounds[1] = midpoint;
+		}
+
+		// 3rd bit of the geohash
+		boolean bit3 = false;
+		midpoint = (bounds[0] + bounds[1]) / 2;
+		if (longitude >= midpoint) {
+			bit3 = true;
+			bounds[0] = midpoint;
+		}
+		else {
+			bit3 = false;
+			bounds[1] = midpoint;
+		}
+		// Continue this process for however many bits of precision we need...
+
+
+		// Faux testing for now
+		assertEquals("100", toHashString(new boolean[] {bit, bit2, bit3}));
+
+		// If you can get the 1D geohash to pass all of these faux tests, you should be in
+		// good shape to complete the 2D version.
+		assertEquals("00000", geohashString(LONGITUDE_RANGE[0], LONGITUDE_RANGE, 5));
+		assertEquals("00000", geohashString(LATITUDE_RANGE[0], LATITUDE_RANGE, 5));
+		assertEquals("11111", geohashString(LONGITUDE_RANGE[1], LONGITUDE_RANGE, 5));
+		assertEquals("11111", geohashString(LATITUDE_RANGE[1], LATITUDE_RANGE, 5));
+		assertEquals("10000", geohashString(0, LONGITUDE_RANGE, 5));
+		assertEquals("11000", geohashString(90.0, LONGITUDE_RANGE, 5));
+		assertEquals("11100", geohashString(135.0, LONGITUDE_RANGE, 5));
+		assertEquals("11110", geohashString(157.5, LONGITUDE_RANGE, 5));
+		assertEquals("11111", geohashString(168.75, LONGITUDE_RANGE, 5));
+		assertEquals("01111", geohashString(-1, LONGITUDE_RANGE, 5));
+		assertEquals("00111", geohashString(-91.0, LONGITUDE_RANGE, 5));
+		assertEquals("00011", geohashString(-136.0, LONGITUDE_RANGE, 5));
+		assertEquals("00001", geohashString(-158.5, LONGITUDE_RANGE, 5));
+		assertEquals("00000", geohashString(-169.75, LONGITUDE_RANGE, 5));
+	}
+}
---
+++ src/main/java/edu/vanderbilt/cs/live2/README.md
@@ -0,0 +1,47 @@
+# Live Session Overview
+
+This programming assignment will exercise your non-OO Java programming
+skills. For those new to Java, this exercise may be very difficult. 
+
+It is OK if this assignment is difficult. Please put in your best effort.
+
+The topics that you will cover in this assignment are:
+
+  1. Recursion
+  2. Implementation of an algorithm from a spec
+  3. Java arrays
+  4. Use of Java system libraries
+  
+If you don't complete the assignment or your implementation has bugs, you
+still need to turn in  your partial / broken implementation to receive
+feedback. However, you should make sure that your work compiles. 
+
+For those new to Java, completing the assignment will likely be an iterative
+process. You do not need to panic if you can't complete the assignment 
+prior to the second live session. There will be time to iterate on your
+code and receive a good grade.
+
+Tip: Commit and push to your repository frequently so that the instructor
+can see your incremental work. Particularly for people that struggle 
+with the assignment, lots of commits will show the effort that you put
+in and raise your score. Also, 
+
+Grading process:
+   1. If you have a working implementation, you will receive a 100
+   2. If you have a broken / incomplete implementation, code comments will
+      be inserted into your work with suggestions on what to improve.
+   3. If you receive comments, you will need to address the comments
+      and commit the updated version within a week of receiving the
+      comments.
+   4. You may receive multiple rounds of comments.
+   5. Once the final version is committed, you will be given a grade.
+   
+What to do:
+   1. See the edu.vanderbilt.cs.live2.GeoHash class for instructions
+   2. When your implementation is complete, make sure the most up
+      to date version is committed and pushed to your assignment
+      repository.
+      
+Warning! There may be typos, bugs, or other issues with the assignment.
+If you encounter a bug, typo, etc., please post to the course discussion
+forum to let the instructor know.
